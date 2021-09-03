1bff6469685d3a140dd8d04c57c332a786733087
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-09-02 23:05:44 -0300

    using userDefaults for save contents of cells

---
+++ To Do/To Do/AssignmentUserDefaults.swift
@@ -0,0 +1,42 @@
+//
+//  AssignmentUserDefauts.swift
+//  To Do
+//
+//  Created by Pedro Léda on 02/09/21.
+//
+
+import UIKit
+
+class AssignmentUserDefauts {
+    // MARK: Properties
+    let key = "listkey"
+    var assignments: [String] = []
+    
+    // MARK: Outlets
+    
+    // MARK: Initialization
+    
+    // MARK: Overrides
+    
+    // MARK: Actions
+    
+    // MARK: Methods
+    func save(assignment: String) {
+        
+        assignments = list()
+        
+        assignments.append(assignment)
+        UserDefaults.standard.set(assignments, forKey: key)
+    }
+    
+    func list() -> Array<String> {
+        let content = UserDefaults.standard.object(forKey: key)
+        
+        if content != nil {
+            return content as! Array<String>
+        } else {
+            return []
+        }
+    }
+
+}
---
+++ To Do/To Do/RegisterViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RegisterViewController.swift
+//  To Do
+//
+//  Created by Pedro Léda on 02/09/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    // MARK: Properties
+    
+    // MARK: Outlets
+    @IBOutlet weak var textFieldAssignment: UITextField!
+    
+    // MARK: Initialization
+    
+    // MARK: Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Actions
+    @IBAction func addAssignment(_ sender: Any) {
+        if let userAssignment = textFieldAssignment.text {
+            let assignment = AssignmentUserDefauts()
+            assignment.save(assignment: userAssignment)
+            textFieldAssignment.text = ""
+        }
+    }
+    
+    // MARK: Methods
+
+
+}
